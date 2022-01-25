f4c719e49c36f20b65bd64b08ba73b7d572cb4e9
Jenni-Suzuran <jenniv_93@hotmail.com>
2022-01-25 20:01:06 +0200

    #2 Heading tags

--- Beatifulsoup works.py
+++ Beatifulsoup works.py
@@ -9,3 +9,14 @@ from bs4 import BeautifulSoup
 raw = BeautifulSoup(html, 'html.parser').get_text()
 tokens = word_tokenize(raw)
 print(tokens)
+
+import requests
+
+url_link = "https://www.foreca.fi/"
+request = requests.get(url_link)
+
+Soup = BeautifulSoup(request.text, 'html.parser')
+
+heading_tags = ["h1", "h2", "h3"]
+for tags in Soup.find_all(heading_tags):
+    print(tags.name + ' -> ' + tags.text.strip())
